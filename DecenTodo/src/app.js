c3b79409565a80c3ef5e2a67e4b48e1bc46888d8
s1nglz <boewer.gil@gmail.com>
2022-02-18 04:46:11 +0100

    display tasks, updateWindow

--- DecenTodo/src/app.js
+++ DecenTodo/src/app.js
@@ -1,5 +1,14 @@
+
+
 app = {
-    contracts: [],
+
+    taskFieldsEnum: {
+        ID: 0,
+        CONTENT: 1,
+        COMPLETED: 2,
+        DATE: 3,
+        DATEINCLUDED: 4,
+    },
 
     load: async () => {
         try {
@@ -7,6 +16,7 @@ app = {
             await app.loadAccount();
             await app.loadContract();
             app.addEventListeners();
+            await app.updateWindow();
             console.log("Loading completed successfully!");
         } catch (error) {
             console.log("Loading failed");
@@ -42,8 +52,8 @@ app = {
             const decenTodo = await $.getJSON('/build/contracts/decenTodo.json');
             const decenTodoContract = TruffleContract(decenTodo);
             decenTodoContract.setProvider(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
-            app.contracts.decenTodo = await decenTodoContract.deployed();
-            if(app.contracts.decenTodo == undefined) throw "Couldn't get smart contract from blockchain"
+            app.decenTodo = await decenTodoContract.deployed();
+            if (app.decenTodo == undefined) throw "Couldn't get smart contract from blockchain"
             console.log("Smart contract loaded successfully");
         } catch (error) {
             console.log("Error occured while loading smart contract: " + error);
@@ -52,19 +62,77 @@ app = {
     },
 
     addEventListeners: () => {
-        $('#addTaskButton').on('click', async () => {
+        $('#addTaskButton').on('click', callbackFunctions.AddTaskButton);
+    },
+
+    //TODO: rename date to datetime
+    updateWindow: async () => {
+        let $tasksList = "";
+        const taskCount = await app.decenTodo.taskCount();
+        for (let i = 0; i < taskCount; i++) {
+
+
+            const dateTimeConverted = new Date(date.toNumber());
+
+            const $tasksListItem = `<li>${content}  -  ${dateIncluded ? "Date-time:" + dateTimeConverted + "  - " : ""} Completed: ${completed ? 'Yes' : 'No'}</li>`;
+            $tasksList += $tasksListItem;
+        }
+        $('#tasksList').append($tasksList);
+    }
+}
+
+const callbackFunctions = {
+    AddTaskButton: async () => {
+        try {
             $contentInput = $('#contentInput');
             $dateInput = $('#dateInput');
             $dateIncludedCb = $('#dateIncludedCb');
 
+            const input = {
+                CONTENT: 0,
+                DATE: 1,
+            }
+
+            let missing = [];
             content = $contentInput.val();
-            console.log(content);
-            date = new Date($dateInput.val());
+            if (content == "" || content == undefined)
+                missing.push(input.CONTENT);
+
+            dateString = $dateInput.val();
+            date = new Date(dateString);
+            if (dateString == "")
+                missing.push(input.DATE);
+
             dateIncluded = $dateIncludedCb.prop('checked');
 
-            await app.contracts.decenTodo.createTask(content, date.getTime(), dateIncluded, { from: app.account });
-        })
-    },
+            if (missing.length > 0) {
+                let missingString = "Please provide all missing information: ";
+                for (let i = 0; i < missing.length; i++) {
+                    let substring = "";
+                    switch (missing[i]) {
+                        case input.CONTENT:
+                            substring += 'content';
+                            break;
+
+                        case input.DATE:
+                            substring += 'date';
+                            break;
+                    }
+
+                    if (i < missing.length - 1)
+                        substring += ', ';
+
+                    missingString += substring;
+                }
+
+                throw missingString;
+            }
+            await app.decenTodo.createTask(content, date.getTime(), dateIncluded, { from: app.account });
+        } catch (error) {
+            console.log(error);
+        }
+        app.updateWindow();
+    }
 }
 
 $(async () => {
